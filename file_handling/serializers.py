c4fb7ed0c195b14332b4cff1152cb6ba985541dd
carloszbent <bentsoft365@gmail.com>
2023-07-25 17:37:44 -0400

    added validation for Files endpoint

--- file_handling/serializers.py
+++ file_handling/serializers.py
@@ -1,6 +1,5 @@
 from .models import File
 from rest_framework import serializers, status
-from rest_framework.validators import ValidationError
 
 
 class FileSerializer(serializers.ModelSerializer):
@@ -9,7 +8,7 @@ class FileSerializer(serializers.ModelSerializer):
     size = serializers.FloatField(required=True)
     file1_contents = serializers.FileField(write_only=True, required=True)
     file2_contents = serializers.FileField(write_only=True, required=False)
-    operation_name = serializers.CharField(max_length=15)
+    operation_name = serializers.CharField(max_length=20)
     number_of_parts = serializers.IntegerField(required=False)
 
     class Meta:
@@ -22,43 +21,3 @@ class FileSerializer(serializers.ModelSerializer):
             "operation_name",
             "number_of_parts"
         ]
-
-        def validate(self, attrs):
-
-            available_operations = [
-                "slice",
-                "divide",
-                "find_common_rows",
-                "find_different_rows",
-                "get_duplicates"
-            ]
-
-            operation_name = attrs.get("operation_name")
-            number_of_parts = attrs.get("number_of_parts")
-            file2_contents = attrs.get("file2_contents")
-            
-            if not operation_name in available_operations:
-                print("wrong opp")
-                raise ValidationError("Invalid Operation Name", status.HTTP_400_BAD_REQUEST)
-            
-            if operation_name == "divide" and not number_of_parts:
-                print("no number of parts")
-                raise ValidationError("Operation is 'divide' but no number of parts was provided", status.HTTP_400_BAD_REQUEST)
-
-            if operation_name == "find_common_rows" or operation_name == "find_different_rows":
-                if not file2_contents:
-                    print("needs 2 files")
-                    raise ValidationError("Operations 'find_common_rows' and 'find_different_rows' require TWO files", status.HTTP_400_BAD_REQUEST)
-
-            if "(text/csv)" not in attrs.get("file1_contents"):
-                print("file1 is not csv")
-                raise ValidationError("File 1 is not a CSV", status.HTTP_400_BAD_REQUEST)
-            
-            if file2_contents:
-                if "(text/csv)" not in file2_contents:
-                    print("file2 is not csv")
-                    raise ValidationError("File 2 is not a CSV", status.HTTP_400_BAD_REQUEST)
-
-            return super().validate(attrs)
-
-
--- file_handling/views.py
+++ file_handling/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.serializers import ValidationError
 from . import serializers
 from .models import File
 
+
+available_operations = [
+    "slice",
+    "divide",
+    "find_common_rows",
+    "find_different_rows",
+    "get_duplicates"
+]
 
 class FilesView(generics.GenericAPIView):
     
     serializer_class = serializers.FileSerializer
 
+    def get_queryset(self):
+        return self.request
+    
+
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        request_data = self.request.data
-        print(request_data)
-        print(request.user)
         if serializer.is_valid():
-            print("valid")
+            # after DRF default validation, apply other custom validations
+            if request.data.get("operation_name") not in available_operations:
+                raise ValidationError({"operation_error":"operation does not exist"}, status.HTTP_400_BAD_REQUEST)
+            if request.data.get("operation_name") == "divide" and not request.data.get("number_of_parts"):
+                raise ValidationError({"number_of_parts_error":"operation 'divide' needs a specific number of parts"}, status.HTTP_400_BAD_REQUEST)
+            if request.data.get("operation_name") == "find_common_rows" or request.data.get("operation_name") == "find_different_rows":
+                if not request.data.get("file2_contents"):
+                    raise ValidationError({"amount_of_files_error":"Operations 'find_common_rows' and 'find_different_rows' require TWO files"}, status.HTTP_400_BAD_REQUEST)
+            if request.data.get("file1_contents").content_type != "text/csv":
+                raise ValidationError({"file_type_error":"File 1 is not a CSV"}, status.HTTP_400_BAD_REQUEST)
+            if request.data.get("file2_contents"):
+                if request.data.get("file2_contents").content_type != "text/csv":
+                    raise ValidationError({"file_type_error":"File 2 is not a CSV"}, status.HTTP_400_BAD_REQUEST)
+
             file_entry = File.objects.create(
                 user=request.user,
                 size=request.data.get("size"),
@@ -26,4 +49,4 @@ class FilesView(generics.GenericAPIView):
             return Response(status=status.HTTP_201_CREATED)
         else:
             print("invalid: ", serializer.errors)
-            return Response({"error": serializer.error}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
